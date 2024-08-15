18b63b514bd1f9c88d17f08300624cc93c4feb9c
sharif khan <64704662+amisharif@users.noreply.github.com>
2024-08-16 00:57:58 +0600

    Update useAxiosSecure.jsx

--- sports/src/components/useAxiosSecure/useAxiosSecure.jsx
+++ sports/src/components/useAxiosSecure/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const axiosSecure  = axios.create({
-    baseURL:'https://server-tau-ashen.vercel.app'
+    baseURL:'https://sports-back-2jl2.onrender.com'
 })
 const useAxiosSecure = () => {
     return axiosSecure;
